eed5354b0db7f51e29992d0ff1a9503761cc69b5
Ty Bandachit <tbandachit@gmail.com>
2020-03-12 00:04:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School
+  attr_accessor :grade
+  def initialize(roster=nil)
+    @roster = Hash.new
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student, grade)
+    if @roster[grade] == nil
+    @roster[grade] = []
+  end
+    @roster[grade] << student
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort
+    @roster.each do |k, v|
+    v.sort!
+    end
+  end
+  
+end 
